ce21ea60f3a0879586122e59081f0998b337aea2
Alekslomyarius <57072019+Alekslomyarius@users.noreply.github.com>
2021-04-14 12:43:41 +0300

    Add files via upload

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,13 +4,18 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth';
 
-export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/dashboard'>
+          <i className='fas fa-user' />{' '}
+          <span className='hide-sm'>Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <a onClick={logout} href='#!'>
-          <i className='fas fa-sign-out-alt'></i>
-          {''}
+          <i className='fas fa-sign-out-alt'></i>{' '}
           <span className='hide-sm'>Logout</span>
         </a>
       </li>
